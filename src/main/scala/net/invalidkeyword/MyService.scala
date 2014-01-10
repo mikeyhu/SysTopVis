3e79abfe512545f1d310e5beb661186736b196c2
Michael Hughes <michael.hughes@springer.com>
2014-01-10 16:13:53 +0000

    Now able to generate diagram based on a few case classes

--- src/main/scala/net/invalidkeyword/MyService.scala
+++ src/main/scala/net/invalidkeyword/MyService.scala
@@ -23,6 +23,28 @@ class MyServiceActor extends Actor with MyService {
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
 
+  val data :List[Service] = List(
+    Service("Core",List(
+      Depends("Identity","a label"),
+      Depends("User-Service")
+    )),
+    Service("Identity"),
+    Service("User-Service")
+  )
+
+  def services = {
+    "\"services\":[" + data.map(s => "\"" + s.name + "\"").mkString(",") + "]"
+  }
+
+  def edges = {
+    val edge = for {
+      service <- data
+      dependencies <- service.depends
+    } yield "{ \"u\": \"" + service.name + "\",     \"v\": \"" + dependencies.on + "\",     \"value\": { \"label\": \"" + dependencies.label + "\" } }"
+
+    "\"edges\":[" + edge.mkString(",") + "]"
+  }
+
   val myRoute =
     path("") {
       get {
@@ -34,37 +56,14 @@ trait MyService extends HttpService {
               </body>
             </html>
           }
+
         }
       }
     }~
     path("services") {
       get {
         complete {
-          """
-            |{
-            |"services":[ "CLOSED", "LISTEN", "SYN RCVD", "SYN SENT","ESTAB", "FINWAIT-1", "CLOSE WAIT", "FINWAIT-2","CLOSING", "LAST-ACK", "TIME WAIT" ],
-            |"edges":[
-            | { "u": "CLOSED",     "v": "LISTEN",     "value": { "label": "open" } },
-            | { "u": "LISTEN",     "v": "SYN RCVD",   "value": { "label": "rcv SYN" } },
-            | { "u": "LISTEN",     "v": "SYN SENT",   "value": { "label": "send" } },
-            | { "u": "LISTEN",     "v": "CLOSED",     "value": { "label": "close" } },
-            | { "u": "SYN RCVD",   "v": "FINWAIT-1",  "value": { "label": "close" } },
-            | { "u": "SYN RCVD",   "v": "ESTAB",      "value": { "label": "rcv ACK of SYN" } },
-            | { "u": "SYN SENT",   "v": "SYN RCVD",   "value": { "label": "rcv SYN" } },
-            | { "u": "SYN SENT",   "v": "ESTAB",      "value": { "label": "rcv SYN, ACK" } },
-            | { "u": "SYN SENT",   "v": "CLOSED",     "value": { "label": "close" } },
-            | { "u": "ESTAB",      "v": "FINWAIT-1",  "value": { "label": "close" } },
-            | { "u": "ESTAB",      "v": "CLOSE WAIT", "value": { "label": "rcv FIN" } },
-            | { "u": "FINWAIT-1",  "v": "FINWAIT-2",  "value": { "label": "rcv ACK of FIN" } },
-            | { "u": "FINWAIT-1",  "v": "CLOSING",    "value": { "label": "rcv FIN" } },
-            | { "u": "CLOSE WAIT", "v": "LAST-ACK",   "value": { "label": "close" } },
-            | { "u": "FINWAIT-2",  "v": "TIME WAIT",  "value": { "label": "rcv FIN" } },
-            | { "u": "CLOSING",    "v": "TIME WAIT",  "value": { "label": "rcv ACK of FIN" } },
-            | { "u": "LAST-ACK",   "v": "CLOSED",     "value": { "label": "rcv ACK of FIN" } },
-            | { "u": "TIME WAIT",  "v": "CLOSED",     "value": { "label": "timeout=2MSL" } }
-            | ]
-            |}
-          """.stripMargin
+           s"{${services},${edges}}"
         }
       }
     }~
@@ -73,3 +72,5 @@ trait MyService extends HttpService {
     }
 }
 
+case class Service(name: String, depends:List[Depends]=List())
+case class Depends(on:String, label:String = "")
